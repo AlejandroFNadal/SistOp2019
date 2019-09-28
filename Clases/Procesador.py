ad3a3914087290acb3fff0a8a9ef8889895282b2
AnastaciusWright <alexandronadal@gmail.com>
2019-09-28 19:25:20 -0300

    metodosprueba

--- Clases/Procesador.py
+++ Clases/Procesador.py
@@ -9,3 +9,7 @@ class Procesador:  # contendra gran parte de las tareas generales
         print("ID    Pro_Tam    Prioridad  Tiempo de Arribo  Secuencia")
         for x in self.procesos_listos:
             x.muestra_proceso()
+    def get_proceso_actual(self):
+        return self.proceso_actual
+    def set_proceso_actual(self,proc)
+        self.proceso_actual=proc
