d677fe9b6a38912f1e367082593129ee7de92a1e
StrideEric <strideeric94@gmail.com>
2019-10-03 07:57:30 -0300

    anadir fija firstFIr

--- Clases/Memoria.py
+++ Clases/Memoria.py
@@ -16,13 +16,19 @@ class Memoria:
                         diferencia = aux
                         best_part = part.get_id_par()
             if best_part != None:
-                part.asignar_proceso(proc)
+                self.lista_particiones[best_part].asignar_proceso(proc) #falta hacer asignar_proceso
             
-            
-            
-                
-                 
-            
+        if self.tipo_particion and self.algoritmo_asignacion == 2: # es fija, FIrstF
+            first_part = None
+            pos = 0
+            while first_part == None and ( pos < len(self.lista_particiones) ):
+                if self.lista_particiones[pos].get_tamano() > proc.get_tamano_proc() and self.lista_particiones[0].get_estado():
+                    first_part = self.lista_particiones[pos].get_id_par()
+                pos += 1                
+            if first_part != None:
+                self.lista_particiones[first_part].asignar_proceso(proc)
+        
+
 class Particion:
     def __init__(self,idp, tamano, dir_in,dir_fin):
         self.id_par=idp
