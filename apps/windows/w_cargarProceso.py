a1b2e568f171b478e48dd8e90e7326b524d929ba
AnastaciusWright <alexandronadal@gmail.com>
2019-10-11 14:16:08 -0300

    Funcion agregar proceso devuelve los datos necesarios, aun no se hace checkeo

--- apps/windows/w_cargarProceso.py
+++ apps/windows/w_cargarProceso.py
@@ -4,6 +4,7 @@ from apps.ui.w_cargarProceso import Ui_cargarProceso
 
 class W_cargarProceso(QMainWindow):
 	def __init__(self, parent=None):
+		self.first_load=True
 		super(W_cargarProceso, self).__init__(parent)
 		#QMainWindow.__init__(self)
 		self.ventana = Ui_cargarProceso()
@@ -16,5 +17,13 @@ class W_cargarProceso(QMainWindow):
 		#ventana = W_cargarProceso()
 		#self.dialogs.append(ventana)
 		#ventana.show()
-		print("hola mudo")
-		print(self.ventana.spinBox_Arribo.value())
+		if self.first_load:
+			nombre=self.ventana.lineEdit_Nombre.text()
+			self.first_load=False
+			self.ventana.lineEdit_Nombre.setEnabled(False)
+		arribo=self.ventana.spinBox_Arribo.value()
+		tamano_proc=self.ventana.sBTamanoProceso.value()
+		prioridad=self.ventana.sB_Prioridad.value()
+		rafaga=self.ventana.lineEdit_rcpu.text()
+		datos=[nombre,arribo,prioridad,tamano_proc,rafaga]
+		print(datos)
