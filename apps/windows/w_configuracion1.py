ca4c58cfd024e6b4a66b7a7c475dacbf8c1887eb
StrideEric <strideeric94@gmail.com>
2019-10-22 22:22:32 -0300

    ampliacion de particionesFijas.py en windows

--- apps/windows/w_configuracion1.py
+++ apps/windows/w_configuracion1.py
@@ -2,6 +2,8 @@ from PyQt5.QtWidgets import QMainWindow
 
 from apps.ui.w_configuracion1 import Ui_configuracion1
 
+from apps.windows.w_particionFija import W_ParticionFija
+
 
 
 class W_Configuracion1(QMainWindow):
@@ -10,3 +12,13 @@ class W_Configuracion1(QMainWindow):
 		#QMainWindow.__init__(self)
 		self.ventana = Ui_configuracion1()
 		self.ventana.setupUi(self)
+		self.dialogs = list()
+		self.ventana.btn_aceptar.clicked.connect(self.config)
+
+
+	def config(self):
+		#if self.radioButtonFija.isCheked(True):
+		ventana = W_ParticionFija()
+		self.dialogs.append(ventana)
+		ventana.show()
+	
---
+++ apps/windows/w_particionFija.py
@@ -0,0 +1,21 @@
+from PyQt5.QtWidgets import QMainWindow
+
+from apps.ui.w_particionFija import Ui_ParticionFija
+from apps.ui.w_configuracion import Ui_Configuracion
+
+class W_ParticionFija(QMainWindow):
+	def __init__(self, parent=None):
+		super(W_ParticionFija, self).__init__(parent)
+		#QMainWindow.__init__(self)
+		self.ventana = Ui_ParticionFija()
+		self.ventana.setupUi(self)
+		"""self.ventana.btn_aceptar.clicked.connect(self.agregarParticion)
+
+	def agregarParticion(self):
+		cant_mem = int(self.ventana.spinBoxTamMemo.text())
+		cant_part = int(self.ventana.spinBox_cantParticion.text())
+		tam_part = int(self.sB_tamParticion.text())
+
+		if cant_part>0 and (cant_mem - (cant_part + tam_part)): #sumo cant part a tamanio de particion para que cada particion tenga como minimo 1 kb
+			pass
+"""
