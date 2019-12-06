c7a360495e8d625d8d555afc6a164f6636d669cf
StrideEric <strideeric94@gmail.com>
2019-12-06 00:10:34 -0300

    correccion de confi y part fijas

--- apps/windows/w_particionFija.py
+++ apps/windows/w_particionFija.py
@@ -73,12 +73,10 @@ class W_ParticionFija(QMainWindow):
 				print("<<< El tamaño colocado para la particion supera el disponible en la memoria >>>")
 
 			if self.cant_part_rest == 1:
-				pass
-				#self.ultima_part()
+				self.ultima_part()
 
 		elif self.cant_part_rest == 1:
-			pass
-			#self.ultima_part()
+			self.ultima_part()
 		else:
 			self.ventana.label_mensaje.setText("Cantidad maxima de particiones colocadas")
 			self.ventana.label_mensaje.setVisible(1)
@@ -104,8 +102,8 @@ class W_ParticionFija(QMainWindow):
 		#Aca deberiamos tener en cuenta si el usuario no completo las particiones.
 		#Podriamos hacer que si pulsa en terminar las particiones se creen automaticas
 		#con igual tamaño todas (o casi todas, la ultima podria tener un poquito mas)
-		
-		tam_part = self.cant_mem_rest//self.cant_part_rest
+		if self.cant_part_rest>0:
+			tam_part = self.cant_mem_rest//self.cant_part_rest
 		#1
 		while self.cant_part_rest > 1:
 			if (tam_part*self.cant_part_rest) == self.cant_part_rest:
@@ -126,9 +124,8 @@ class W_ParticionFija(QMainWindow):
 
 		#2
 		if self.cant_part_rest == 1:
-			pass
-			#self.ultima_part()
-		
+			self.ultima_part()
+
 		self.pasar_datos()
 		self.close()
 
